398a4dfe16555738a858e219ea06bd330168aad4
Daryn Haffer <dhaffer1991@gmail.com>
2021-01-12 01:58:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// FORK this starter file and save it to your own Repl.it account.
+
+
+// Declare and initialize the 12 variables here:
+let date = 'Monday 2019-03-18';
+let time = '10:05:34 AM';
+let astronautCount = 7;
+let astronautStatus = 'ready';
+let averageAstronautMassKg = 80.7;
+let crewMassKg = (astronautCount * averageAstronautMassKg);
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = (crewMassKg + fuelMassKg + shuttleMassKg);
+let fuelTempCelsius = -225;
+let fuelLevel = '100%';
+let weatherStatus = 'clear';
+
+
+
+// Write code to generate the LC04 report here:
+let LC04 = 
+`> LC04 - LAUNCH CHECKLIST
+-------------------------------------
+Date: ${date}
+Time: ${time}
+
+-------------------------------------
+> ASTRONAUT INFO
+-------------------------------------
+* count: ${astronautCount}
+* status: ${astronautStatus}
+
+-------------------------------------
+> FUEL DATA
+-------------------------------------
+* Fuel temp celsius: ${fuelTempCelsius}
+* Fuel level: ${fuelLevel}
+
+-------------------------------------
+> MASS DATA
+-------------------------------------
+* Crew mass: ${crewMassKg}
+* Fuel mass: ${fuelMassKg}
+* Shuttle mass: ${shuttleMassKg}
+* Total mass: ${totalMassKg}
+
+-------------------------------------
+> FLIGHT PLAN
+-------------------------------------
+* weather: ${weatherStatus}
+
+-------------------------------------
+> OVERALL STATUS
+-------------------------------------
+* Clear for takeoff: YES
+`;
+
+
+
+// When done, have your TA check your code.
+console.log(LC04);
+
+// BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
